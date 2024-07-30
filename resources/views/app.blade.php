6febcfe34bf84f9bb4a96712a267301a70263ead
NayKhuKham <Jthawit@gmail.com>
2024-07-30 13:47:50 +0700

    first commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>POS-01</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <h1>Hello POS BY LARAVEL 10</h1>
+    <div id="app-vue"></div>
+
+
+    @vite('resources/js/app.js')
+</body>
+</html>
