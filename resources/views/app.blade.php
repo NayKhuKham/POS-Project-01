7823b36d4ac79e348881614297e72db01836ccc7
NayKhuKham <Jthawit@gmail.com>
2024-07-31 13:28:32 +0700

    second Commit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,13 +5,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>POS-01</title>
+
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="assets/vendor/css/core.css" class="template-customizer-core-css">
+    <link rel="stylesheet" href="assets/vendor/css/theme-default.css" class="template-customizer-theme-css">
+    <link rel="stylesheet" href="assets/css/demo.css">
+    {{-- <link rel="stylesheet" href="assets/css/font_lao.css"> --}}
+    <!-- Vendors CSS -->
+    <link rel="stylesheet" href="assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css"> 
+    <link rel="stylesheet" href="assets/vendor/libs/apex-charts/apex-charts.css">
+
+    <!-- Boxicon CSS -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
     @vite('resources/css/app.css')
 </head>
 <body>
-    <h1>Hello POS BY LARAVEL 10</h1>
     <div id="app-vue"></div>
-
-
+      <!-- Core JS -->
+  <!-- build:js assets/vendor/js/core.js -->
+  
+  <script src="assets/vendor/libs/jquery/jquery.js"></script>
+  <script src="assets/vendor/libs/popper/popper.js"></script>
+  <script src="assets/vendor/js/bootstrap.js"></script>
+  <script src="assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+  <script src="assets/vendor/js/menu.js"></script>
+  
+  <!-- endbuild -->
     @vite('resources/js/app.js')
 </body>
 </html>
